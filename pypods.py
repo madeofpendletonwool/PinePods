0bc39880e07418d60dbd55628d0a5bf32538e9f0
brickinthewall10 <blacanwite@gmail.com>
2022-12-19 21:28:33 -0600

    Adding flet pypods - removing uneeded

--- pypods.py
+++ pypods.py
@@ -1,12 +1,47 @@
-#!/bin/python
+import flet as ft
+import time
 
-from flask import Flask, render_template
+def main(page: ft.Page):
+    page.title = "pyPods - A python based podcast app!"
+    
+    
+    # page.controls.append(testtx)
+    # page.update()
+
+    def search_podcast(e):
+        if not search_pods.value:
+            search_pods.error_text = "Please enter a podcast to seach for - Cannot search nothing"
+            page.update()   
+        else:
+            podcast_value = search_pods.value
+            page.clean()
+            page.add(ft.Text(f"Searching for {podcast_value}!"))
+
+    # Page Header
+
+    header = ft.Container(
+        content=ft.Text(value="pyPods", color="blue", size=50, expand=True),
+        alignment=ft.alignment.center
+        # podsear_btn
+
+    )
+
+    # Podcast Search Function
+
+    search_pods = ft.TextField(label="Search for new podcast", content_padding=5, width=200)
+    search_box = ft.Container(
+        content=search_pods,
+        alignment=ft.alignment.top_right
+    )
+    search_btn = ft.ElevatedButton("Search!", on_click=search_podcast)
+    search_btn_ctn = ft.Container(
+        content=search_btn,
+        alignment=ft.alignment.top_right
+    )
+
+    page.add(
+        ft.Row([header, search_box, search_btn_ctn])
+    )
 
 
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-app.run(host="0.0.0.0", port=80)
+ft.app(target=main, port=8034)
