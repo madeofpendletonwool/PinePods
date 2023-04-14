6768d6f7833df42dc424ed052729fe9a5ff46398
Collin Pendleton <collinp@collinpendleton.com>
2023-04-14 08:25:59 -0500

    Compose file, setupdatabase, and startup script updated

--- startup/setupdatabase.py
+++ startup/setupdatabase.py
@@ -1,79 +1,150 @@
 import mysql.connector
 import os
 
-print(os.getcwd())
+# Database variables
+db_host = os.environ.get("DB_HOST", "127.0.0.1")
+db_port = os.environ.get("DB_PORT", "3306")
+db_user = os.environ.get("DB_USER", "root")
+db_password = os.environ.get("DB_PASSWORD", "password")
+db_name = os.environ.get("DB_NAME", "pypods_database")
 
-# Connect to the database
+# Create database connector
 cnx = mysql.connector.connect(
-    host="127.0.0.1",
-    port="3306",
-    user="root",
-    password="password",
-    database="pypods_database"
+    host=db_host,
+    port=db_port,
+    user=db_user,
+    password=db_password,
+    database=db_name,
+    charset='utf8mb4'
 )
 
-# Create a cursor object
+# create a cursor to execute SQL statements
 cursor = cnx.cursor()
 
-# Read the SQL script file into a string
-with open("startup/tablecreate.sql", "r") as file:
-    table_setup = file.read()
-
-# Execute the SQL script
-# Create the Users table
-cursor.execute("""CREATE TABLE Users (
-                  UserID INT AUTO_INCREMENT PRIMARY KEY,
-                  Username TEXT,
-                  Email VARCHAR(255),
-                  Hashed_PW CHAR(60),
-                  Salt CHAR(60)
+# create tables
+cursor.execute("""CREATE TABLE IF NOT EXISTS Users (
+                    UserID INT AUTO_INCREMENT PRIMARY KEY,
+                    Fullname TEXT,
+                    Username TEXT,
+                    Email VARCHAR(255),
+                    Hashed_PW CHAR(60),
+                    Salt CHAR(60),
+                    IsAdmin TINYINT(1)
                 )""")
 
-# Create the Podcasts table
-cursor.execute("""CREATE TABLE Podcasts (
-                  PodcastID INT AUTO_INCREMENT PRIMARY KEY,
-                  PodcastName TEXT,
-                  ArtworkURL TEXT,
-                  Author TEXT,
-                  Categories TEXT,
-                  Description TEXT,
-                  EpisodeCount INT,
-                  FeedURL TEXT,
-                  WebsiteURL TEXT,
+cursor.execute("""CREATE TABLE IF NOT EXISTS UserStats (
+                    UserStatsID INT AUTO_INCREMENT PRIMARY KEY,
+                    UserID INT,
+                    UserCreated TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    PodcastsPlayed INT DEFAULT 0,
+                    TimeListened INT DEFAULT 0,
+                    PodcastsAdded INT DEFAULT 0,
+                    EpisodesSaved INT DEFAULT 0,
+                    EpisodesDownloaded INT DEFAULT 0,
+                    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+                )""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS AppSettings (
+                    AppSettingsID INT AUTO_INCREMENT PRIMARY KEY,
+                    SelfServiceUser TINYINT(1) DEFAULT 0
+                )""")
+
+cursor.execute("""INSERT INTO AppSettings (SelfServiceUser)
+                  SELECT 0 FROM DUAL WHERE NOT EXISTS (SELECT * FROM AppSettings)""")
+
+cursor.execute("""INSERT INTO Users (Fullname, Username, Email, Hashed_PW, Salt, IsAdmin)
+                    VALUES ('Guest User', 'guest', 'inactive', 'Hmc7toxfqLssTdzaFGiKhigJ4VN3JeEy8VTkVHQ2FFrxAg74FrdoPRXowqgh', 'Hmc7toxfqLssTdzaFGiKhigJ4VN3JeEy8VTkVHQ2FFrxAg74FrdoPRXowqgh', 0)
+                    ON DUPLICATE KEY UPDATE UserID=UserID""")
+
+cursor.execute("""INSERT IGNORE INTO UserStats (UserID) VALUES (1)""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS Podcasts (
+                    PodcastID INT AUTO_INCREMENT PRIMARY KEY,
+                    PodcastName TEXT,
+                    ArtworkURL TEXT,
+                    Author TEXT,
+                    Categories TEXT,
+                    Description TEXT,
+                    EpisodeCount INT,
+                    FeedURL TEXT,
+                    WebsiteURL TEXT,
+                    UserID INT,
+                    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+                )""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS Episodes (
+                    EpisodeID INT AUTO_INCREMENT PRIMARY KEY,
+                    PodcastID INT,
+                    EpisodeTitle TEXT,
+                    EpisodeDescription TEXT,
+                    EpisodeURL TEXT,
+                    EpisodeArtwork TEXT,
+                    EpisodePubDate DATE,
+                    EpisodeDuration INT,
+                    FOREIGN KEY (PodcastID) REFERENCES Podcasts(PodcastID)
+                )""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS UserSettings (
+                    UserSettingID INT AUTO_INCREMENT PRIMARY KEY,
+                    UserID INT,
+                    Theme VARCHAR(255) DEFAULT 'nordic',
+                    FOREIGN KEY (UserID) REFERENCES Users(UserID)
+                )""")
+
+cursor.execute("""INSERT INTO UserSettings (UserID, Theme) VALUES ('1', 'nordic')""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS UserEpisodeHistory (
+                    UserEpisodeHistoryID INT AUTO_INCREMENT PRIMARY KEY,
+                    UserID INT,
+                    EpisodeID INT,
+                    ListenDate DATETIME,
+                    ListenDuration INT,
+                    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+                    FOREIGN KEY (EpisodeID) REFERENCES Episodes(EpisodeID)
+                )""")
+
+cursor.execute("""CREATE TABLE IF NOT EXISTS SavedEpisodes (
+                    SaveID INT AUTO_INCREMENT PRIMARY KEY,
+                    UserID INT,
+                    EpisodeID INT,
+                    SaveDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                    FOREIGN KEY (UserID) REFERENCES Users(UserID),
+                    FOREIGN KEY (EpisodeID) REFERENCES Episodes(EpisodeID)
+                )""")
+
+
+# Create the DownloadedEpisodes table
+cursor.execute("""CREATE TABLE IF NOT EXISTS DownloadedEpisodes (
+                  DownloadID INT AUTO_INCREMENT PRIMARY KEY,
                   UserID INT,
-                  FOREIGN KEY (UserID) REFERENCES Users(UserID)
-                )""")
-
-# Create the Episodes table
-cursor.execute("""CREATE TABLE Episodes (
-                  EpisodeID INT AUTO_INCREMENT PRIMARY KEY,
-                  PodcastID INT,
-                  EpisodeTitle TEXT,
-                  EpisodeDescription TEXT,
-                  EpisodeURL TEXT,
-                  EpisodePubDate DATE,
-                  EpisodeDuration INT,
-                  FOREIGN KEY (PodcastID) REFERENCES Podcasts(PodcastID)
-                )""")
-
-# Create the UserSettings table
-cursor.execute("""CREATE TABLE UserSettings (
-                  UserSettingID INT AUTO_INCREMENT PRIMARY KEY,
-                  UserID INT,
-                  SettingName TEXT,
-                  SettingValue TEXT,
-                  FOREIGN KEY (UserID) REFERENCES Users(UserID)
-                )""")
-
-# Create the EpisodeProgress table
-cursor.execute("""CREATE TABLE EpisodeProgress (
-                  EpisodeProgressID INT AUTO_INCREMENT PRIMARY KEY,
                   EpisodeID INT,
+                  DownloadedDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                  DownloadedSize INT,
+                  DownloadedLocation VARCHAR(255),
+                  FOREIGN KEY (UserID) REFERENCES Users(UserID),
+                  FOREIGN KEY (EpisodeID) REFERENCES Episodes(EpisodeID)
+                )""")
+
+# Create the EpisodeQueue table
+cursor.execute("""CREATE TABLE IF NOT EXISTS EpisodeQueue (
+                  QueueID INT AUTO_INCREMENT PRIMARY KEY,
+                  QueueDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                   UserID INT,
-                  EpisodeProgress INT,
-                  FOREIGN KEY (EpisodeID) REFERENCES Episodes(EpisodeID),
+                  EpisodeID INT,
+                  QueuePosition INT NOT NULL DEFAULT 0,
+                  FOREIGN KEY (UserID) REFERENCES Users(UserID),
+                  FOREIGN KEY (EpisodeID) REFERENCES Episodes(EpisodeID)
+                )""")
+
+# Create the Sessions table
+cursor.execute("""CREATE TABLE IF NOT EXISTS Sessions (
+                  SessionID INT AUTO_INCREMENT PRIMARY KEY,
+                  UserID INT,
+                  value TEXT,
+                  expire DATETIME NOT NULL,
                   FOREIGN KEY (UserID) REFERENCES Users(UserID)
                 )""")
+
 
 # Close the cursor
 cursor.close()
@@ -81,6 +152,5 @@ cursor.close()
 # Commit the changes
 cnx.commit()
 
-# Close the cursor and connection
-cursor.close()
+# Close the connection
 cnx.close()
--- startup/startup.sh
+++ startup/startup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-python3 /startup/setupdatabase.py
+export DB_USER=$DB_USER
+export DB_PASSWORD=$DB_PASSWORD
+export DB_HOST=$DB_HOST
+export DB_NAME=$DB_NAME
+export FULLNAME=$FULLNAME
+export USERNAME=$USERNAME
+export EMAIL=$EMAIL
+export PASSWORD=$PASSWORD
+
+python3 /pinepods/startup/setupdatabase.py
+python3 /pinepods/create_user.py $DB_USER $DB_PASSWORD $DB_HOST $DB_NAME $FULLNAME $USERNAME $EMAIL $PASSWORD
+
+python3 /pinepods/pypods.py
