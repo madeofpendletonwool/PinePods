f86d93068aa5c93d1656fc93632d8368b1d5b17a
Collin Pendleton <collinp@collinpendleton.com>
2023-04-26 20:46:21 -0500

    debug pw statements

--- clients/Auth/Passfunctions.py
+++ clients/Auth/Passfunctions.py
@@ -21,6 +21,7 @@ def hash_password(password: str):
 
 def verify_password(cnx, username: str, password: str) -> bool:
     cursor = cnx.cursor()
+    print('checking pw')
     cursor.execute("SELECT Hashed_PW, Salt FROM Users WHERE Username = %s", (username,))
     result = cursor.fetchone()
     cursor.close()
@@ -30,8 +31,13 @@ def verify_password(cnx, username: str, password: str) -> bool:
     hashed_password = result[0].encode('utf-8')
     salt = result[1].encode('utf-8')
 
+    print(f"Stored hashed_password: {hashed_password}")
+    print(f"Stored salt: {salt}")
+
     # Hash the password with the stored salt
     password_hash = bcrypt.hashpw(password.encode('utf-8'), salt)
+
+    print(f"Generated password_hash: {password_hash}")
 
     # Compare the hashed password with the stored hash
     return password_hash == hashed_password
