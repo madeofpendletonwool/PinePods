a3b21a4983d163b27c196a72e4e879c9f5e0057d
Collin Pendleton <collinp@collinpendleton.com>
2023-06-03 21:27:14 -0500

    Error handling only in test_connection

--- internal_functions/functions.py
+++ internal_functions/functions.py
@@ -17,39 +17,26 @@ def test_connection(api_url):
 
 def searchpod(podcast_value, api_url):
     # Set the query parameter
-    params = {'query': podcast_value}
+    params = {'query': f'{podcast_value}'}
+
+    # Make the GET request to the API
+    response = requests.get(api_url, params=params)
 
     try:
-        # Make the GET request to the API
-        response = requests.get(api_url, params=params)
-        response.raise_for_status()  # raise exception if invalid HTTP status code received
-    except requests.exceptions.HTTPError as http_err:
-        return f"HTTP error occurred: {http_err}"
-    except requests.exceptions.ConnectionError as conn_err:
-        return f"Error connecting to the server: {conn_err}"
-    except requests.exceptions.RequestException as req_err:
-        return f"Error occurred: {req_err}"
-
-    # Only decode the response if there is content
-    if response.content:
-        try:
-            search_results = response.json()
-            return search_results
-        except ValueError as e:
-            return f"Error decoding JSON: {e}"
-    else:
-        return f"Received empty response from the server"
-
-
+        search_results = response.json()
+        return search_results
+    except:
+        search_results = response.status_code
+        return search_results
 
 if __name__ == '__main__':
     api_url = 'https://search.pinepods.online/api/search'
     podcast_value = 'my brother my brother and me'
     results = searchpod(podcast_value, api_url)
     print(results)
-    # if isinstance(results, str):
-    #     print(f"Error occurred: {results}")
-    # else:
-    #     return_results = results.get('feeds', [])
-    #     for d in return_results:
-    #         print(d.get('title', ''))
+    if isinstance(results, str):
+        print(f"Error occurred: {results}")
+    else:
+        return_results = results.get('feeds', [])
+        for d in return_results:
+            print(d.get('title', ''))
