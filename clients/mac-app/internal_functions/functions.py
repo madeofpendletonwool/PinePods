51a861cfde0129ec978469cc952e4ed54fe05eeb
Collin Pendleton <collinp@collinpendleton.com>
2023-06-23 14:37:49 -0500

    Update all internal functions

--- clients/mac-app/internal_functions/functions.py
+++ clients/mac-app/internal_functions/functions.py
@@ -15,10 +15,9 @@ def test_connection(api_url):
     # If there's no exception, the connection is established successfully
 
 
-def searchpod(podcast_value, api_url):
-
+def searchpod(podcast_value, api_url, search_index='podcastindex'):
     # Set the query parameter
-    params = {'query': f'{podcast_value}'}
+    params = {'query': f'{podcast_value}', 'index': search_index}
 
     # Make the GET request to the API
     response = requests.get(api_url, params=params)
@@ -31,14 +30,20 @@ def searchpod(podcast_value, api_url):
         return search_results
 
 
+
 if __name__ == '__main__':
-    api_url = 'https://search.pinepods.online/api/search'
-    podcast_value = 'Me and My Brothers Podcast'
-    results = searchpod(podcast_value, api_url)
+    api_url = 'http://localhost:5000/api/search'
+    podcast_value = 'my brother my brother and me'
+    results = searchpod(podcast_value, api_url, 'itunes')
     print(results)
     if isinstance(results, str):
         print(f"Error occurred: {results}")
     else:
-        return_results = results.get('feeds', [])
-        for d in return_results:
-            print(d.get('title', ''))
+        if 'results' in results:  # if iTunes API was used
+            for d in results['results']:
+                print(d.get('trackName', ''))
+        else:  # if PodcastIndex API was used
+            return_results = results.get('feeds', [])
+            for d in return_results:
+                print(d.get('title', ''))
+
