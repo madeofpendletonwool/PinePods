543ea6ba5503a3e1b19c01aa367c3719fbe485b0
Collin Pendleton <collinp@collinpendleton.com>
2023-08-10 22:58:20 -0500

    Adding api key header

--- startup/call_refresh_endpoint.sh
+++ startup/call_refresh_endpoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Ensure app has time to start
-sleep 10
+sleep 15
 
 # Read the API key from /tmp/web_api_key.txt
 API_KEY=$(cat /tmp/web_api_key.txt)
 
 # Call the FastAPI endpoint using the API key
 echo "Refreshing now!"
-curl "http://localhost:8032/api/data/refresh_pods?api_key=$API_KEY" >> /cron.log 2>&1
+curl "http://localhost:8032/api/data/refresh_pods" -H "Authorization: $API_KEY" >> /cron.log 2>&1
