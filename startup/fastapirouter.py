b0ed134bfd9a51cc34a9ec5dd75fe4a4d464d9c3
Collin Pendleton <collinp@collinpendleton.com>
2023-08-17 06:30:06 -0500

    Adding in websockets to router

--- startup/fastapirouter.py
+++ startup/fastapirouter.py
@@ -1,9 +1,10 @@
-from fastapi import FastAPI, Request, HTTPException, Response
+from fastapi import FastAPI, Request, HTTPException, Response, WebSocket
 import httpx
 import logging
+import websockets
 
 app = FastAPI()
-logging.basicConfig(level=logging.DEBUG)
+logging.basicConfig(level=logging.INFO)
 
 @app.api_route("/{path:path}", methods=["GET", "POST", "PUT", "DELETE"])
 async def proxy_requests(request: Request, path: str):
@@ -54,6 +55,35 @@ async def proxy_requests(request: Request, path: str):
         except httpx.RequestError as exc:
             logging.error(f"An error occurred while requesting {exc.request.url!r}.")
             raise HTTPException(status_code=500, detail="Internal Server Error")
+
+import asyncio
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    # You can also add authentication here if needed.
+    remote_websocket = await websockets.connect("ws://localhost:8034/ws")
+    try:
+        async def forward(local_ws, remote_ws):
+            while True:
+                data = await local_ws.receive_text()
+                await remote_ws.send(data)
+
+        async def backward(local_ws, remote_ws):
+            while True:
+                remote_data = await remote_ws.recv()
+                await local_ws.send_text(remote_data)
+
+        await asyncio.gather(forward(websocket, remote_websocket), backward(websocket, remote_websocket))
+
+    except websockets.ConnectionClosed as e:
+        logging.info(f"Connection closed: {e}")
+    except Exception as e:
+        logging.error(f"Error in websocket: {e}")
+    finally:
+        await websocket.close()
+        await remote_websocket.close()
+
 
 if __name__ == '__main__':
     import uvicorn
