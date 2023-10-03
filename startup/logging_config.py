e7e2238e822c04cb5bfa72ec794841adf7322d9b
Collin Pendleton <collinp@collinpendleton.com>
2023-10-03 07:33:57 -0500

    logging dictionary setup

--- startup/logging_config.py
+++ startup/logging_config.py
@@ -1,18 +1,36 @@
-import logging
 import os
 
-DEBUG = os.environ.get("DEBUG_MODE", "False") == "True"
-
-print(f"Logging options set to debug: {DEBUG}")
+DEBUG_MODE = os.environ.get("DEBUG_MODE", "False") == "True"
+LOGGING_LEVEL = "DEBUG" if DEBUG_MODE else "ERROR"
 
 LOGGING_CONFIG = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "format": "[%(asctime)s] [%(levelname)s] - %(name)s: %(message)s",
+        }
+    },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
-            "level": "DEBUG" if DEBUG else "ERROR",
-        }
+            "level": LOGGING_LEVEL,
+            "formatter": "default",
+            "stream": "ext://sys.stdout",
+        },
     },
-    "root": {"level": "DEBUG" if DEBUG else "ERROR", "handlers": ["console"]},
+    "loggers": {
+        "uvicorn": {
+            "level": LOGGING_LEVEL,
+            "handlers": ["console"],
+        },
+        "fastapi": {
+            "level": LOGGING_LEVEL,
+            "handlers": ["console"],
+        },
+        "your_application_logger_name": {  # Replace with your logger name
+            "level": LOGGING_LEVEL,
+            "handlers": ["console"],
+        },
+    },
 }
