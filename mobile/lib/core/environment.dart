8f99b9ba54c59435fe0e28c79317cf719a49b7f6
GitHub Action <action@github.com>
2026-06-26 12:10:49 +0000

    chore: update version to 0.9.0 [skip ci]

--- mobile/lib/core/environment.dart
+++ mobile/lib/core/environment.dart
@@ -33,7 +33,7 @@ class Environment {
   static const _applicationUrl =
       'https://github.com/madeofpendletonwool/pinepods';
   static const _projectVersion = '0.9.0';
-  static const _build = '20252487';
+  static const _build = '20252489';
 
   static var _agentString = userAgentAppString;
 
